bb7d9505cefd7e299b89f6b07b783920d9cf97aa
Naveen Gowda MY <naveengowda23277@gmail.com>
2025-03-11 16:24:01 +0530

    pushed startup code

---
+++ src/pages/CompanyDashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CompanyDashboard = () => {
+  return <div>DashBoard</div>;
+};
+
+export default CompanyDashboard;
---
+++ src/pages/CompanyJobDashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CompanyJobDashboard = () => {
+  return <div>Job Dashboard</div>;
+};
+
+export default CompanyJobDashboard;
